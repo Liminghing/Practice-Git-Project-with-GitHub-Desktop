eea9e8d7bd8fec6641f7e478909a941704bf7f63
Liminghing <im349412@gmail.com>
2024-11-18 12:42:23 +0900

    edit on testBranch

--- src/main/java/com/example/practicegithubdesktop/Main.kt
+++ src/main/java/com/example/practicegithubdesktop/Main.kt
@@ -5,7 +5,7 @@ class Main {
     //edit project
     var myString = "edit project"
     //testBranch에서 작업
-    var newString = "testBranch"
+    var testBranchString = "testBranch"
 
     //기존브랜치(master)에서 작업
     var masterString = "working on master"
