57514f58271a34323069425af44f754809f1410e
Liminghing <im349412@gmail.com>
2024-11-19 11:01:22 +0900

    second edit on master branch

--- src/main/java/com/example/practicegithubdesktop/TestGitHub.kt
+++ src/main/java/com/example/practicegithubdesktop/TestGitHub.kt
@@ -2,6 +2,7 @@ package com.example.practicegithubdesktop
 
 class TestGitHub {
     //master브랜치에서 수정
-    var String = "master브랜치에서수정"
-    
+    var String01 = "master브랜치에서수정"
+    //master브랜치에서 수정2
+    var String02 = "master브랜치에서수정2"
 }
